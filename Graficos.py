1e85c6363340757be9bc60330db58bebd3944ede
VahosDev <juandavidvahosbohorquez4@gmail.com>
2025-08-03 20:56:49 -0500

    feat: Add initial HTML structure, CSS styles, and LinkedIn SVG icon
    - Created a new index.html file with a structured layout including header, main content, and footer.
    - Added a navigation menu with links and a LinkedIn icon.
    - Introduced a new CSS file for styling, defining variables and styles for body, header, and navigation.
    - Included a LinkedIn SVG icon for external link in the navigation.

--- Graficos.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-import pandas as pd
-import matplotlib as plt
-
-url = "https://github.com/JuanDaVahos/Practice/raw/refs/heads/main/Entradas_de_extranjeros_a_Colombia_20250803.csv"
-
-df = pd.DataFrame(url, sep=",")
