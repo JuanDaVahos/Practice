8f9d38499b9aa534339b25873867d380134528dc
VahosDev <juandavidvahosbohorquez4@gmail.com>
2025-08-04 20:04:04 -0500

    Add toggle functionality for menu icon in another.js

---
+++ js/another.js
@@ -0,0 +1,11 @@
+const checkboxMenu = document.querySelector("#Menu");
+const labelMenu = document.querySelector(".iconMenu");
+const iconImg = labelMenu.querySelector("img");
+
+labelMenu.addEventListener("click", () => {
+  if (checkboxMenu.checked) {
+    iconImg.src = "../svg/menu-close.svg";
+  } else {
+    iconImg.src = "../svg/menu-open.svg";
+  }
+});
